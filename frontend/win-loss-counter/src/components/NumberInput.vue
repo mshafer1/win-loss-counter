0f5eca8a224a9948837cd1ab512e06ef10aa4c89
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-07-06 22:12:59 -0500

    get better number input on mobile

--- frontend/win-loss-counter/src/components/NumberInput.vue
+++ frontend/win-loss-counter/src/components/NumberInput.vue
@@ -5,7 +5,19 @@ defineEmits(['change'])
 <template>
   <div class="w3-xxlarge">
     <p v-text="label"></p>
-    <input class="w3-input w3-xxlarge" type="number" v-model="value" @input="onChanged()" />
+    <span class="w3-hide-small w3-hide-medium">
+      <input class="w3-input w3-xxlarge" type="number" v-model="value" @input="onChanged()" />
+    </span>
+    <span class="w3-row-flex w3-hide-large">
+      <div class="w3-col s8 w3-border w3-border-black large-text">
+      {{ value }}
+      </div>
+      <div class="w3-col s4">
+        <button class="w3-button w3-gray" @click="up">&#x25B2;</button>
+        <br/>
+        <button class="w3-button w3-gray" @click="down">&#x25BC;</button>
+      </div>
+    </span>
   </div>
 </template>
 
@@ -30,6 +42,16 @@ export default {
     }
   },
   methods: {
+    up() {
+      this.olValue = this.value;
+      this.value += 1;
+      this.$emit('change', this.label.toLowerCase(), this.value);
+    },
+    down() {
+      this.olValue = this.value;
+      this.value -= 1;
+      this.$emit('change', this.label.toLowerCase(), this.value);
+    },
     onChanged(event) {
       if (this.value == this.oldValue) {
         return
